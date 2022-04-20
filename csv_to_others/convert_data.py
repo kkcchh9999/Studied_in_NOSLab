017850145873f345610ba7cb6ed1ae9f1f0cfb12
kkcchh9999 <kkcchh9999@naver.com>
2022-04-20 16:45:30 +0900

    journal

--- csv_to_others/convert_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-#csv 파일 읽기 
-data = pd.read_csv("/home/kch/mnist/mnist.csv", index_col=0)
-
-#csv to json
-data.to_json("/home/kch/mnist.json")
-
--- csv_to_others/read_hdd_ssd.py
+++
@@ -1,5 +0,0 @@
-import time
-
-cur_time = time.time()
-mnist_ssd = open("/home/kch/mnist/mnist.xlsx", "r")
-print("ssd: ", time.time()-cur_time) 
